da094c41f009ced1a8a997c78ce07539bb60f6ae
AdrianoOllie <adrianoollie@gmail.com>
2022-05-28 11:41:38 -0300

    encurtador de url

---
+++ src/config/Constants.ts
@@ -0,0 +1,5 @@
+export const config = {
+	API_URL: 'http://localhost:5000',
+	MONGO_CONNECTION:
+		'mongodb+srv://adrianoollie:<password>@cluster0.pntwy.mongodb.net/?retryWrites=true&w=majority',
+}
